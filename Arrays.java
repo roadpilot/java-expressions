29e729a4421e805ddb19413729157fa3155494cf
roadpilot <mdacht777@gmail.com>
2021-11-15 14:16:51 -0700

    array of strings

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,8 @@
 public class Arrays {
+    // arrays elements have to all be the same data type
     public static void main(String[] args){
-        System.out.println("test");
+        // System.out.println("test");
+        String[] arr = {"a","b","c"};
+        System.out.println(arr.length);
     }      
 }
