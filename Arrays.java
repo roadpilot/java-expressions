2ae362c817242ae7ffd177f6b0fb4239ec762b9a
roadpilot <mdacht777@gmail.com>
2021-11-15 14:23:18 -0700

    initialize methods

--- Arrays.java
+++ Arrays.java
@@ -3,6 +3,12 @@ public class Arrays {
     public static void main(String[] args){
         // System.out.println("test");
         String[] arr = {"a","b","c"};
-        System.out.println(arr.length);
+        // System.out.println(arr.length);
+        int[] ages = new int[3];
+        ages[0] = 39;
+        ages[1] = 42;
+        
+        int[] scores = {99, 65, 78};
+        
     }      
 }
